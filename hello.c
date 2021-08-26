a8d4a69ecb978b11e709d2ad54b0fec44aa0b0de
Katelyn Breitbarth <kbreitbarth2@huskers.unl.edu>
2021-08-26 19:12:26 +0000

    added my information

--- hello.c
+++ hello.c
@@ -1,7 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 
  int main(int argc, char **argv) {
 
         printf("Henry Xiao\n");
         printf("My major is Comp sci\n");
+        printf("Katelyn Breitbarth\n");
+        printf("My major is Undeclared\n");
         return 0;
  }
